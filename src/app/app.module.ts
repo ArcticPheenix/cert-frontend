ba270197abb96560f5dc87dd0ced0a62c812c5d5
ArcticPheenix <phoenix.ckelly@gmail.com>
2018-04-18 13:38:50 -0600

    Critical functionality imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CsrFormComponent } from './csr-form/csr-form.component';
@@ -12,7 +14,10 @@ import { CsrFormComponent } from './csr-form/csr-form.component';
     CsrFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
